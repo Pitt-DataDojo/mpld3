4c3e8adc6a2cb09652f34c00fd3d45b2b304a282
Zak Risha <zjrisha@gmail.com>
2018-10-20 18:24:55 -0400

    working version

--- server/index.py
+++ server/index.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, jsonify
-import os
-import seaborn as sns
-# import numpy as np
 import pandas as pd
-import matplotlib as mpl
+import matplotlib
+matplotlib.use("agg")
 import matplotlib.pyplot as plt, mpld3
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/api/dataviz', methods=['GET'])
 def data_viz():
     fig, ax = plt.subplots()
     ax.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
     payload = mpld3.fig_to_dict(fig)
-    return jsonify(payload)
+    jsonData = jsonify(payload)
+    return jsonData
